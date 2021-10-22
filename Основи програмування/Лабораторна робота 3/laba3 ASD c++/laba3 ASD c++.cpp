c004c3cbf0f64911093b6fc8b6ac2b3430f24151
Denial-First <76117612+Denial-First@users.noreply.github.com>
2021-10-22 15:50:51 +0300

    Add files via upload

---
+++ Основи програмування/Лабораторна робота 3/laba3 ASD c++/laba3 ASD c++.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <iostream>
+//#include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    double x, con, p, a, eps, xp1, xp2, xp3, xp4, xp5;
+    x = 1;
+    cout << "x = a^(1/p)" << endl;
+
+    cout << "a = ";
+    cin >> a;
+    
+    cout << "p = ";
+    cin >> p;
+
+    while (p == 1 || p == 2) {
+        cout << "Invalid item. Try again" << endl;
+        cout << "p =";
+        cin >> p;
+    }
+    
+    cout << "Epsilon = ";
+    cin >> eps;
+    
+    con = pow(a, 1 / p);
+   /* while(abs(con - x) > eps)
+    {       
+        xp1 = (x / (p * p));
+        xp2 = (p * p - 1);
+        xp3 = (0.5 * (p + 1) * a / pow(x, p));
+        xp4 = (0.5 * (p + 1) * pow(x, p) / p);
+        xp5 = xp2 + xp3 - xp4;
+
+        x = xp1 * floor(xp5);
+        cout << x << endl;
+    } */
+    while (abs(con - x) > eps) 
+    {
+        xp1 = 1 / p;
+        xp2 = (p - 1) * x;
+        xp3 = a / pow(x, p - 1);
+        x = xp1 * (xp2 + xp3);
+    }
+    cout << "x = " << x;
+}
---
+++ Основи програмування/Лабораторна робота 4/laba4/laba4.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int i, k;
+    float x, res;
+    double num, den;
+    num = 1;
+    den = 1;
+    
+    cout << "x = ";
+    cin >> x;
+    
+    for (i = 2; i<=64; i = 2 * i) 
+    {
+        while (x == i - 1) {
+            cout << "There is no result. Try again" << endl;
+            cout << "x = ";
+            cin >> x;
+        }
+        num = num * (x - i);
+    }
+    for (k = 1; k <= 63; k = 2 * k + 1)
+    {
+        den = den * (x - k);
+    }
+    res = num / den;
+    cout << "result = " << res;
+}
